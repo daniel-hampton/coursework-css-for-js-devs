6ec5ab8eecd0df0022d16816929e4d73c2eafb86
Daniel Hampton <dhampton084@gmail.com>
2023-01-26 16:53:25 -0500

    init module 02-character-creator

---
+++ 02-character-creator/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import CharacterEditor from "./components/CharacterEditor";
+import Footer from "./components/Footer";
+// Hello world
+function App() {
+  return (
+    <>
+      <CharacterEditor />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
